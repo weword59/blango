d133270fbe973dec86296baec66cbabf053c6806
Bill Word <weword@gmail.com>
2023-12-09 18:40:14 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for blango project.
 
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
