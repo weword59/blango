8906c7e56688466ae1171fc79478f72565b2039c
Bill Word <weword@gmail.com>
2023-12-22 14:02:35 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    
+    logger.debug("Got %d posts", len(posts))
+
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {"post": post})
+    
+    
+    logger.info(
+        "Created comment on Post %d for user %s", post.pk, request.user
+    )
+    return render(request, "blog/index.html", {"post": post})
