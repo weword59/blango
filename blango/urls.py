a72bf0f51e07a477a59a05817e1ec3e3599cda43
Bill Word <weword@gmail.com>
2023-12-30 14:34:41 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("ip/", blog.views.get_ip),
+    path("api/v1/", include("blog.api_urls")),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
 
